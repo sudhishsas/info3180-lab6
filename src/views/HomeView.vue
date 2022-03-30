9deee0dca848cf181338f1831cac6c44545ca308
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2022-03-30 12:22:00 -0500

    added a new component to components called newslist and imported it to the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
 export default {
-    data() {
-        return {
-            message: "Hello World! Welcome to VueJS"
-        };
-    },
+ data() {
+ return {
+ message: "Hello World! Welcome to VueJS"
+ };
+ },
+ components: { NewsList }
 }
 </script>
 
@@ -12,11 +14,14 @@ export default {
     <div class="container">
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
+        <h1>{{ message }} </h1>
+        <NewsList/>
       </div>
+        
     </div>
 </template>
 
 <style>
 /* Add any component specific styles here */
+
 </style>
