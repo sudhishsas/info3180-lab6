fd4430e0b80b59421c0fb5607db67ad148c62edc
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2022-03-31 20:25:43 -0500

    added a new rout called news and functionality to the nav bar for navigating to this new page created

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,6 +23,9 @@
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink to="/news" class="nav-link">News</RouterLink>
+            </li>
           </ul>
         </div>
       </div>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="news container">
+    <h2>News</h2>
+ </div>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+<style>
+/* Add any component specific styles here */
+</style>
